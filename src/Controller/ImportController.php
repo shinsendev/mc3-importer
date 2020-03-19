279a10ec173d3587cc1e29e289a7097e8d361227
gdarquie <gaetan.darquie@gmail.com>
2020-03-19 23:24:07 +0100

    Add file

---
+++ src/Controller/ImportController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Component\MySQL\Clean\MySQLClean;
+use App\Component\MySQL\Import\MySQLImport;
+use App\Component\MySQL\Initialization\MySQLInitialization;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ImportController
+ * @package App\Controller
+ */
+class ImportController extends AbstractController
+{
+    /**
+     * @Route("/import/all", name="import_all")
+     */
+    public function all()
+    {
+        MySQLInitialization::init();
+        MySQLImport::import('../data/mc2.sql');
+        MySQLClean::clean();
+
+        $this->addFlash('success', 'Everything is ok');
+        return $this->forward('App\Controller\DefaultController::index');
+    }
+ 
+    /**
+     * @Route("/import/init", name="import_init")
+     */
+    public function init()
+    {
+        MySQLInitialization::init();
+
+        $this->addFlash('success', 'Initialisation ok');
+        return $this->forward('App\Controller\DefaultController::index');
+    }
+
+    /**
+     * @Route("/import/creation", name="import_creation")
+     */
+    public function create()
+    {
+        MySQLImport::import('../data/mc2.sql');
+
+        $this->addFlash('success', 'DB structure has been created and data has been imported');
+        return $this->forward('App\Controller\DefaultController::index');
+    }
+
+    /**
+     * @Route("/import/clean", name="import_clean")
+     */
+    public function clean()
+    {
+        MySQLClean::clean();
+        $this->addFlash('success', 'DB has been cleaned.');
+        return $this->forward('App\Controller\DefaultController::index');
+    }
+
+    /**
+     * @Route("/import/upgrade", name="import_upgrade")
+     */
+    public function upgrade()
+    {
+        return $this->forward('App\Controller\DefaultController::index');
+    }
+
+    /**
+     * @Route("/import/export", name="import_export")
+     */
+    public function export()
+    {
+        return $this->forward('App\Controller\DefaultController::index');
+    }
+    
+}
