0868a6f70e6ad227da36b0fcc12fb504e71939a2
gdarquie <gaetan.darquie@gmail.com>
2020-05-12 23:23:12 +0200

    MC-7 add Uuid and date to film

--- src/Component/Migration/ImportFilms.php
+++ src/Component/Migration/ImportFilms.php
@@ -6,6 +6,7 @@ namespace App\Component\Migration;
 
 use App\Component\MySQL\Connection\MySQLConnection;
 use App\Component\PostgreSQL\Connection\PostgreSQLConnection;
+use Ramsey\Uuid\Uuid;
 
 class ImportFilms
 {
@@ -30,25 +31,36 @@ class ImportFilms
 
         // the we insert the films one by one
         foreach ($films as $film) {
-            $sql = "INSERT INTO film (title, uuid, production_year, released_year, imdb, length, remake) VALUES (:title, '10b7a5d6-70cd-4d0a-ba12-56d8744e46db', :productionyear, :released, :imdb, :length, :remake)";
-            $rsl = $psql->prepare($sql);
-            $rsl->execute([
-                // set correct values
-                'title' => ($film['title']) ? $film['title'] : null,
-                'productionyear' => ($film['productionyear']) ? $film['productionyear'] : null,
-                'released' => ($film['released']) ? $film['released'] : null,
-                'imdb' => ($film['id_imdb']) ? $film['id_imdb'] : null,
-                'length' => ($film['length']) ? $film['length'] : null,
-                'remake' => ($film['remake']) ? $film['remake'] : null,
-            ]);
+            self::insertFilm($psql, $film);
         }
-
-        dd($films[0]);
 
         return true;
     }
 
-    static public function insertFilm() {
-        //todo
+    /**
+     * @param $connection
+     * @param $film
+     */
+    static public function insertFilm($connection, $film)
+    {
+        $uuid = Uuid::uuid4()->toString();
+        $date = new \DateTime();
+        $date = $date->format('Y-m-d H:i:s');
+
+        $sql = "INSERT INTO film (title, uuid, production_year, released_year, imdb, length, remake, pca, created_at, updated_at) VALUES (:title, :uuid, :productionYear, :released, :imdb, :length, :remake, :pca, :createdAt, :updatedAt)";
+        $rsl = $connection->prepare($sql);
+        $rsl->execute([
+            // set correct values
+            'title' => ($film['title']) ? $film['title'] : null,
+            'productionYear' => ($film['productionyear']) ? $film['productionyear'] : null,
+            'released' => ($film['released']) ? $film['released'] : null,
+            'imdb' => ($film['id_imdb']) ? $film['id_imdb'] : null,
+            'length' => ($film['length']) ? $film['length'] : null,
+            'remake' => ($film['remake']) ? $film['remake'] : null,
+            'pca' => ($film['pca_texte']) ? $film['pca_texte'] : null,
+            'createdAt' => $date,
+            'updatedAt' => $date,
+            'uuid' => $uuid
+        ]);
     }
 }
