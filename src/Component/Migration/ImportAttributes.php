0a3a68570d3d4677b4de04f7fee7433ab1624e67
gdarquie <gaetan.darquie@gmail.com>
2020-05-13 18:03:59 +0200

    MC-7: add categories to attributes

--- src/Component/Migration/ImportAttributes.php
+++ src/Component/Migration/ImportAttributes.php
@@ -6,6 +6,7 @@ declare(strict_types=1);
 namespace App\Component\Migration;
 
 
+use App\Component\MySQL\Connection\MySQLConnection;
 use Ramsey\Uuid\Uuid;
 
 class ImportAttributes
@@ -16,9 +17,20 @@ class ImportAttributes
         $date = new \DateTime();
         $date = $date->format('Y-m-d H:i:s');
 
-        // todo: add category
+        // get the correct category id and add it in the insert
+        // get code content = the name in MySQL db
+        $mysql = MySQLConnection::connection();
+        $rsl = $mysql->prepare('SELECT * FROM code WHERE code_id = :code');
+        $rsl->execute(['code' => $thesaurus['code_id']]);
+        $code = $rsl->fetch()['content'];
 
-        $sql = "INSERT INTO attribute (title, description, example, uuid, created_at, updated_at) VALUES (:title, :definition, :example, :uuid, :createdAt, :updatedAt)";
+        // get the category id in PostgreSQL db
+        $rsl = $connection->prepare('SELECT id FROM category WHERE code = :code');
+        $rsl->execute(['code' => $code]);
+        $categoryId = $rsl->fetch()['id'];
+
+        // insert MySQL data into PostgreSQL
+        $sql = "INSERT INTO attribute (title, description, example, uuid, created_at, updated_at, category_id) VALUES (:title, :definition, :example, :uuid, :createdAt, :updatedAt, :categoryId)";
         $rsl = $connection->prepare($sql);
         $rsl->execute([
             // set correct values
@@ -27,7 +39,8 @@ class ImportAttributes
             'example' => ($thesaurus['example']) ? $thesaurus['example'] : null,
             'createdAt' => ($thesaurus['date_creation'] && $thesaurus['date_creation'] > 0 ) ? $thesaurus['date_creation'] : $date,
             'updatedAt' => ($thesaurus['last_update'] && $thesaurus['date_creation'] > 0) ? $thesaurus['last_update'] : $date,
-            'uuid' => $uuid
+            'uuid' => $uuid,
+            'categoryId' => $categoryId,
         ]);
 
         //todo : add comment
