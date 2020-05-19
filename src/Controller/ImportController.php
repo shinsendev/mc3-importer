82072de2b50e9734ca8769a0b99822c7354bdd43
gdarquie <gaetan.darquie@gmail.com>
2020-05-19 18:07:23 +0200

    Finish import all relations thesaurus for number & upgdrade import all

--- src/Controller/ImportController.php
+++ src/Controller/ImportController.php
@@ -59,9 +59,54 @@ class ImportController extends AbstractController
         MigrationHelper::importRelations('film_has_studio', 'film_studio', 'film', 'studio',1000);
 
         // import censorship
-//        ImportAttributes::importExternalThesaurusString('censorship', 'censorship', 'film_has_censorship', 'film', 1000 );
+        ImportAttributes::importExternalThesaurusString('state', 'state', 'film_has_state', 'film_attribute', 'film', 1000, true );
 
         // import states
+        ImportAttributes::importExternalThesaurusString('state', 'state', 'film_has_state', 'film_attribute', 'film', 1000, true );
+
+        // import number attributes
+
+        // import complet_options
+        ImportAttributes::importRelationsForExistingAttributes('number_has_completoptions', 'number_attribute', 'complet_options', 'number', 'attribute', 'number_id', 'completoptions_id',  1000);
+
+        // import dancecontent
+        ImportAttributes::importRelationsForExistingAttributes('number_has_dancecontent', 'number_attribute', 'dance_content', 'number', 'attribute', 'number_id', 'dancecontent_id',  1000);
+
+        // import dancemble
+        ImportAttributes::importRelationsForExistingAttributes('number_has_dancemble', 'number_attribute', 'dancemble', 'number', 'attribute', 'number_id', 'dancemble_id',  1000);
+
+        // import dancesubgenre
+        ImportAttributes::importRelationsForExistingAttributes('number_has_dancesubgenre', 'number_attribute', 'dance_subgenre', 'number', 'attribute', 'number_id', 'dancesubgenre_id',  1000);
+
+        // dancingtype
+        ImportAttributes::importRelationsForExistingAttributes('number_has_dancingtype', 'number_attribute', 'dancing_type', 'number', 'attribute', 'number_id', 'dancingtype_id',  1000);
+
+        // diegeticplace
+        ImportAttributes::importRelationsForExistingAttributes('number_has_diegeticplace', 'number_attribute', 'diegetic_place_thesaurus', 'number', 'attribute', 'number_id', 'diegetic_place_thesaurus_id',  1000);
+
+        // exoticismthesaurus
+        ImportAttributes::importRelationsForExistingAttributes('number_has_exoticismthesaurus', 'number_attribute', 'exoticism_thesaurus', 'number', 'attribute', 'number_id', 'exoticism_thesaurus_id',  1000);
+
+        // genre => topic
+        ImportAttributes::importRelationsForExistingAttributes('number_has_genre', 'number_attribute', 'genre', 'number', 'attribute', 'number_id', 'genre_id',  1000);
+
+        // imaginary
+        ImportAttributes::importRelationsForExistingAttributes('number_has_imaginary', 'number_attribute', 'imaginary', 'number', 'attribute', 'number_id', 'imaginary_id',  1000);
+
+        // musensemble
+        ImportAttributes::importRelationsForExistingAttributes('number_has_musensemble', 'number_attribute', 'musensemble', 'number', 'attribute', 'number_id', 'musensemble_id',  1000);
+
+        // musicalthesaurus
+        ImportAttributes::importRelationsForExistingAttributes('number_has_musicalthesaurus', 'number_attribute', 'musical_thesaurus', 'number', 'attribute', 'number_id', 'musical_thesaurus_id',  1000);
+
+        // quotationthesaurus
+        ImportAttributes::importRelationsForExistingAttributes('number_has_quotationthesaurus', 'number_attribute', 'quotation_thesaurus', 'number', 'attribute', 'number_id', 'quotation_id',  1000);
+
+        // source
+        ImportAttributes::importRelationsForExistingAttributes('number_has_source', 'number_attribute', 'source_thesaurus', 'number', 'attribute', 'number_id', 'source_thesaurus_id',  1000);
+
+        // stereotype
+        ImportAttributes::importRelationsForExistingAttributes('number_has_stereotype', 'number_attribute', 'stereotype_thesaurus', 'number', 'attribute', 'number_id', 'stereotype_id',  1000);
 
         $this->addFlash('success', 'Everything is ok');
         return $this->redirectToRoute('home');
@@ -247,24 +292,34 @@ class ImportController extends AbstractController
         ImportAttributes::importRelationsForExistingAttributes('number_has_dancesubgenre', 'number_attribute', 'dance_subgenre', 'number', 'attribute', 'number_id', 'dancesubgenre_id',  1000);
 
         // dancingtype
+        ImportAttributes::importRelationsForExistingAttributes('number_has_dancingtype', 'number_attribute', 'dancing_type', 'number', 'attribute', 'number_id', 'dancingtype_id',  1000);
 
         // diegeticplace
+        ImportAttributes::importRelationsForExistingAttributes('number_has_diegeticplace', 'number_attribute', 'diegetic_place_thesaurus', 'number', 'attribute', 'number_id', 'diegetic_place_thesaurus_id',  1000);
 
         // exoticismthesaurus
+        ImportAttributes::importRelationsForExistingAttributes('number_has_exoticismthesaurus', 'number_attribute', 'exoticism_thesaurus', 'number', 'attribute', 'number_id', 'exoticism_thesaurus_id',  1000);
 
         // genre => topic
+        ImportAttributes::importRelationsForExistingAttributes('number_has_genre', 'number_attribute', 'genre', 'number', 'attribute', 'number_id', 'genre_id',  1000);
 
         // imaginary
+        ImportAttributes::importRelationsForExistingAttributes('number_has_imaginary', 'number_attribute', 'imaginary', 'number', 'attribute', 'number_id', 'imaginary_id',  1000);
 
         // musensemble
+        ImportAttributes::importRelationsForExistingAttributes('number_has_musensemble', 'number_attribute', 'musensemble', 'number', 'attribute', 'number_id', 'musensemble_id',  1000);
 
         // musicalthesaurus
+        ImportAttributes::importRelationsForExistingAttributes('number_has_musicalthesaurus', 'number_attribute', 'musical_thesaurus', 'number', 'attribute', 'number_id', 'musical_thesaurus_id',  1000);
 
         // quotationthesaurus
+        ImportAttributes::importRelationsForExistingAttributes('number_has_quotationthesaurus', 'number_attribute', 'quotation_thesaurus', 'number', 'attribute', 'number_id', 'quotation_id',  1000);
 
         // source
+        ImportAttributes::importRelationsForExistingAttributes('number_has_source', 'number_attribute', 'source_thesaurus', 'number', 'attribute', 'number_id', 'source_thesaurus_id',  1000);
 
         // stereotype
+        ImportAttributes::importRelationsForExistingAttributes('number_has_stereotype', 'number_attribute', 'stereotype_thesaurus', 'number', 'attribute', 'number_id', 'stereotype_id',  1000);
 
         return $this->redirectToRoute('home');
     }
